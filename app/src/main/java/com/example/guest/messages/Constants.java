41f2bb4ebee48bfa800d34c2d622ff2299adb1ed
Danyel Murray and Ross Fletcher <danyel@>
2018-03-28 13:29:06 -0700

    add working firebase saving messages

--- app/src/main/java/com/example/guest/messages/Constants.java
+++ app/src/main/java/com/example/guest/messages/Constants.java
@@ -2,5 +2,7 @@ package com.example.guest.messages;
 
 
 public class Constants {
-    public static final String PREFERENCES_MESSAGE_KEY = "message";
+//    public static final String PREFERENCES_MESSAGE_KEY = "message";
+    public static final String FIREBASE_CHILD_MESSAGE = "message";
+
 }
--- app/src/main/java/com/example/guest/messages/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/messages/ui/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.guest.messages.Constants;
 import com.example.guest.messages.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,20 +21,29 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.etCreateMessage) EditText etCreateMessageLabel;
     @BindView(R.id.bSubmitMessage) Button bSubmitMessageLabel;
+    private String mMessage;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
 
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+    private DatabaseReference mMessageRef;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mMessageRef = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_MESSAGE);
+
         super.onCreate(savedInstanceState);
         setContentView(com.example.guest.messages.R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//
+//        mEditor = mSharedPreferences.edit();
 
         etCreateMessageLabel.setOnClickListener(this);
         bSubmitMessageLabel.setOnClickListener(this);
@@ -41,13 +53,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         if(v == bSubmitMessageLabel){
             String message = etCreateMessageLabel.getText().toString();
-            addToSharedPreferences(message);
-
+            saveMessageToFirebase(message);
+//            System.out.println(message);
+//            addToSharedPreferences(message);
+//            mMessage = mSharedPreferences.getString(Constants.PREFERENCES_MESSAGE_KEY, null);
+//            Log.d("Shared Pref Location", mMessage);
         }
-
+    }
+    public void saveMessageToFirebase(String message) {
+        mMessageRef.push().setValue(message);
     }
 
-    private void addToSharedPreferences(String message) {
-        mEditor.putString(Constants.PREFERENCES_MESSAGE_KEY, message).apply();
-}
+//    private void addToSharedPreferences(String message) {
+//        mEditor.putString(Constants.PREFERENCES_MESSAGE_KEY, message).apply();
+//}
 }
